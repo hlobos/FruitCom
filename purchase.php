ffc4c96f3116976dc5d52204d45aa6d78a7197ac
Hazel Lobos <hazel@lobos.ca>
2017-09-19 04:13:22 -0600

    makePurchase now updates vendor CSV quantities

--- purchase.php
+++ purchase.php
@@ -39,8 +39,8 @@
           $new_contents .= $new_line;
         }
 		  } else {
-			  if($record !== '') {
-				$new_contents .= $record . "\n";
+			    if($record !== '') {
+				    $new_contents .= $record . "\n";
 			  }
 		  }
 	  }
@@ -50,5 +50,44 @@
 	file_put_contents($dir, $new_contents);
   
 	/* --- Update vendor-....csv with new quantity information --- */
+  $vendorId = strtolower($vendorName);              //Lowercase
+  $vendorId = str_replace("'", "", $vendorId);      //Remove single quotes
+  $vendorId = str_replace(".", "", $vendorId);      //Remove periods
+  $vendorId = preg_replace('/\s+/', '', $vendorId); //Remove whitespace
+  
+  $vendor_dir = "database/vendor-" . $vendorId . ".csv";
 
+	//fruit as key
+	$vendor_key = $fruitName; 
+
+	//New line with quantity changed
+  $activeStatus = "true";
+  
+  if($newQuantityAvailable == 0) {
+      $activeStatus = "false";
+  }
+  
+	$vendor_new_line = $fruitName . "," . $newQuantityAvailable . "," . $buyPrice . "," . $buyPrice . "," . $activeStatus . "\n";
+
+	$vendor_contents = file_get_contents($vendor_dir);
+
+	$vendor_new_contents = "";
+  
+	if(strpos($vendor_contents, $vendor_key) !== false) {
+	  $vendor_contents_array = preg_split("/\\r\\n|\\r|\\n/", $vendor_contents);
+	  
+	  foreach ($vendor_contents_array as &$vendor_record) {   
+		  if (strpos($vendor_record, $vendor_key) !== false) {
+          $vendor_new_contents .= $vendor_new_line;
+		  } else {
+			    if($vendor_record !== '') {
+            $vendor_new_contents .= $vendor_record . "\n";
+          }
+		  }
+	  }
+	}
+
+	// Save the new records to file
+	file_put_contents($vendor_dir, $vendor_new_contents);
+  
 ?>
